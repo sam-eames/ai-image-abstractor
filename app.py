3527b96011a403fabdc76bfc5ccaae47e0721d40
Sam Eames <sam.eames@au.ey.com>
2024-11-19 12:16:34 +1100

    re add full app

--- app.py
+++ app.py
@@ -1,104 +1,102 @@
-# import streamlit as st
-# from io import BytesIO
-# from PIL import Image
-# import spacy
-# import spacy_wordnet
-# from spacy_wordnet.wordnet_annotator import WordnetAnnotator
+import streamlit as st
+from io import BytesIO
+from PIL import Image
+import spacy
+import spacy_wordnet
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator
 
 
 
-# from image_generation import (authenticate_genai_client, authenticate_vision_client,
-#                               prompt_to_image, image_loop, load_image_from_url, DEFAULT_SIZE)
+from image_generation import (authenticate_genai_client, authenticate_vision_client,
+                              prompt_to_image, image_loop, load_image_from_url, DEFAULT_SIZE)
 
 
-# # util functions
-# def display_starter_image(image_bytes):
-#     st.header('Starting Image:')
+# util functions
+def display_starter_image(image_bytes):
+    st.header('Starting Image:')
     
-#     image_file = BytesIO(image_bytes)
-#     image = Image.open(image_file).resize(DEFAULT_SIZE)
-#     left_co, cent_co, right_co = st.columns(3)
-#     with cent_co:
-#         st.image(image)
+    image_file = BytesIO(image_bytes)
+    image = Image.open(image_file).resize(DEFAULT_SIZE)
+    left_co, cent_co, right_co = st.columns(3)
+    with cent_co:
+        st.image(image)
 
-# # Page setup
-# st.title('Image Abstraction Looper')
+# Page setup
+st.title('Image Abstraction Looper')
 
-# with st.sidebar:
-#     st.subheader('Configuration')
-#     num_loops = st.number_input("Select number of loops", value = 1,
-#                                 min_value = 1, max_value = 5)
-#     beginning_option = st.radio('Input selection', ['Text', 'Image'])
-#     caption_mode = st.radio('Dense Captions?', ['Regular', 'Dense'])
-#     prompt_randomisation = st.checkbox('Randomise prompts?', True)
+with st.sidebar:
+    st.subheader('Configuration')
+    num_loops = st.number_input("Select number of loops", value = 1,
+                                min_value = 1, max_value = 5)
+    beginning_option = st.radio('Input selection', ['Text', 'Image'])
+    caption_mode = st.radio('Dense Captions?', ['Regular', 'Dense'])
+    prompt_randomisation = st.checkbox('Randomise prompts?', True)
     
-#     nlp = None
-#     if prompt_randomisation:
-#         num_to_replace = st.number_input("Select number of words to replace",
-#                                         value = 1, min_value = 1, max_value = 50)
+    nlp = None
+    if prompt_randomisation:
+        num_to_replace = st.number_input("Select number of words to replace",
+                                        value = 1, min_value = 1, max_value = 50)
         
-#         # set up spacy pipeline
-#         nlp = spacy.load('en_core_web_md')
-#         nlp.add_pipe("spacy_wordnet", after='tagger')
+        # set up spacy pipeline
+        nlp = spacy.load('en_core_web_md')
+        nlp.add_pipe("spacy_wordnet", after='tagger')
 
 
-# # Authenticate/setup
-# genai_client = authenticate_genai_client()
-# vision_client = authenticate_vision_client()
+# Authenticate/setup
+genai_client = authenticate_genai_client()
+vision_client = authenticate_vision_client()
 
-# image_bytes = None
-# start_loop = None
+image_bytes = None
+start_loop = None
 
-# # Set up initial image
-# st.header('Initial Input:')
+# Set up initial image
+st.header('Initial Input:')
 
-# if beginning_option == 'Image':
-#     initial_image = st.file_uploader('Provide an initial image')
+if beginning_option == 'Image':
+    initial_image = st.file_uploader('Provide an initial image')
 
-#     if initial_image is not None:
-#         image_bytes = initial_image.getvalue()
-#         display_starter_image(image_bytes)        
+    if initial_image is not None:
+        image_bytes = initial_image.getvalue()
+        display_starter_image(image_bytes)        
         
-# elif beginning_option == 'Text':
-#     initial_prompt = st.text_input('Provide an initial prompt')
+elif beginning_option == 'Text':
+    initial_prompt = st.text_input('Provide an initial prompt')
 
-# # if inputs valid, display start button
-# if (beginning_option == 'Image'
-#     or (beginning_option == 'Text' and initial_prompt != '')):
-#     start_loop = st.button('Start Running')
+# if inputs valid, display start button
+if (beginning_option == 'Image'
+    or (beginning_option == 'Text' and initial_prompt != '')):
+    start_loop = st.button('Start Running')
 
-# if start_loop:
-#     # for prompt inputs, generate first image
-#     if beginning_option == 'Text':
-#         image_url = prompt_to_image(genai_client, initial_prompt)
-#         image_bytes = load_image_from_url(image_url)
-#         display_starter_image(image_bytes)
+if start_loop:
+    # for prompt inputs, generate first image
+    if beginning_option == 'Text':
+        image_url = prompt_to_image(genai_client, initial_prompt)
+        image_bytes = load_image_from_url(image_url)
+        display_starter_image(image_bytes)
 
-#     st.header('Results')
+    st.header('Results')
     
-#     for i in range(num_loops):
+    for i in range(num_loops):
 
-#         # overwrite current_caption, use image_bytes as input for next loop
-#         current_caption, image_prompt, image_bytes = image_loop(vision_client, genai_client,
-#                                                                 image_bytes, caption_mode, nlp,
-#                                                                 num_to_replace)
+        # overwrite current_caption, use image_bytes as input for next loop
+        current_caption, image_prompt, image_bytes = image_loop(vision_client, genai_client,
+                                                                image_bytes, caption_mode, nlp,
+                                                                num_to_replace)
         
-#         image_file = BytesIO(image_bytes)
-#         current_image = Image.open(image_file).resize(DEFAULT_SIZE)
+        image_file = BytesIO(image_bytes)
+        current_image = Image.open(image_file).resize(DEFAULT_SIZE)
 
-#         # display results for each loop
-#         with st.spinner():
-#             st.subheader(f'Loop {i+1} Result')
-#             left_co, right_co = st.columns(2)
+        # display results for each loop
+        with st.spinner():
+            st.subheader(f'Loop {i+1} Result')
+            left_co, right_co = st.columns(2)
             
-#             with left_co:
-#                 st.caption('Image')
-#                 st.image(current_image)
+            with left_co:
+                st.caption('Image')
+                st.image(current_image)
 
-#             with right_co:
-#                 st.caption('Caption')
-#                 st.write(current_caption)
-#                 st.caption('Prompt')
-#                 st.write(image_prompt)
-
-print('hello world')
+            with right_co:
+                st.caption('Caption')
+                st.write(current_caption)
+                st.caption('Prompt')
+                st.write(image_prompt)
--- nlp_utils.py
+++ nlp_utils.py
@@ -1,48 +1,48 @@
-# import spacy
-# from spacy.matcher import Matcher
-# from spacy_wordnet.wordnet_annotator import WordnetAnnotator
-# import random
+import spacy
+from spacy.matcher import Matcher
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator
+import random
 
-# def replace_word(nlp, initial_text, word_to_replace, synonym):
+def replace_word(nlp, initial_text, word_to_replace, synonym):
 
-#     matcher = Matcher(nlp.vocab)
-#     matcher.add(word_to_replace, [[{"LOWER": word_to_replace}]])
+    matcher = Matcher(nlp.vocab)
+    matcher.add(word_to_replace, [[{"LOWER": word_to_replace}]])
     
-#     tok = nlp(initial_text)
-#     text = ''
-#     buffer_start = 0
-#     for _, match_start, _ in matcher(tok):
-#         if match_start > buffer_start:
-#             text += tok[buffer_start: match_start].text + tok[match_start - 1].whitespace_
-#         text += synonym + tok[match_start].whitespace_
-#         buffer_start = match_start + 1
-#     text += tok[buffer_start:].text
-#     return text
+    tok = nlp(initial_text)
+    text = ''
+    buffer_start = 0
+    for _, match_start, _ in matcher(tok):
+        if match_start > buffer_start:
+            text += tok[buffer_start: match_start].text + tok[match_start - 1].whitespace_
+        text += synonym + tok[match_start].whitespace_
+        buffer_start = match_start + 1
+    text += tok[buffer_start:].text
+    return text
 
-# def replace_random_tokens(nlp, text, num_items):
-#     doc = nlp(text)
-#     noun_adj_list = [token for token in doc if token.pos_ in ['NOUN', 'ADJ', 'VERB']]
+def replace_random_tokens(nlp, text, num_items):
+    doc = nlp(text)
+    noun_adj_list = [token for token in doc if token.pos_ in ['NOUN', 'ADJ', 'VERB']]
 
-#     num_noun_adjs = len(noun_adj_list)
-#     if num_items > num_noun_adjs:
-#         num_items = num_noun_adjs
+    num_noun_adjs = len(noun_adj_list)
+    if num_items > num_noun_adjs:
+        num_items = num_noun_adjs
 
-#     random_noun_adjs = random.sample(noun_adj_list, num_items)
+    random_noun_adjs = random.sample(noun_adj_list, num_items)
 
-#     for token_to_replace in random_noun_adjs:
-#         # select random synset from list of synsets
-#         synsets = token_to_replace._.wordnet.synsets()
+    for token_to_replace in random_noun_adjs:
+        # select random synset from list of synsets
+        synsets = token_to_replace._.wordnet.synsets()
 
-#         # if word has synonyms, replace
-#         if synsets:
-#             synonyms = random.choice(synsets).lemma_names()
+        # if word has synonyms, replace
+        if synsets:
+            synonyms = random.choice(synsets).lemma_names()
             
-#             # select random synonym from synset list
-#             synonym = random.choice(synonyms[1:]) if len(synonyms) > 1 else synonyms[0]
-#             synonym = synonym.replace('_', ' ')
-#             # synonyms[1] if len(synonyms) > 1 else synonyms[0]
+            # select random synonym from synset list
+            synonym = random.choice(synonyms[1:]) if len(synonyms) > 1 else synonyms[0]
+            synonym = synonym.replace('_', ' ')
+            # synonyms[1] if len(synonyms) > 1 else synonyms[0]
             
-#             word_to_replace = str(token_to_replace)
-#             text = replace_word(nlp, text, word_to_replace, synonym)
+            word_to_replace = str(token_to_replace)
+            text = replace_word(nlp, text, word_to_replace, synonym)
 
-#     return text
+    return text
